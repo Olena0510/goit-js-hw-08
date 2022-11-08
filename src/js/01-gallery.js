beec77b1f9011b3fd17eb7a88c3e7d3442ee505f
Olena Kameneva <lenakr0910@gmail.com>
2022-11-08 23:54:36 +0300

    homework_8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// console.log(galleryItems);
 
-console.log(galleryItems);
+
+const galleryListEl = document.querySelector(".gallery");
+
+const addItemGallery = galleryItems.map(({ preview, original, description }) =>
+    `<a class="gallery__item" 
+href="${original}">
+  <img class="gallery__image" 
+  src="${preview}" 
+  alt="${description}" />
+</a>`
+);
+
+galleryListEl.insertAdjacentHTML('beforeend', addItemGallery.join(''));
+
+new SimpleLightbox('.gallery a', {
+  captionsData: "alt",
+  captionDelay: 250, });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from "lodash.throttle";
+
+const STORAGE_KEY = 'feedback-form-state';
+const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY))
+const formData = {...savedMessage};
+
+const inputRef = document.querySelector('input');
+const formRef = document.querySelector('.feedback-form');
+const textareaRef = document.querySelector('textarea');
+
+
+formRef.addEventListener('submit', onFormSubmit);
+formRef.addEventListener('input', throttle(onForm, 500));
+
+getText();
+
+function onForm(evt) {
+    formData[evt.target.name] = evt.target.value
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault()
+    evt.currentTarget.reset()
+    localStorage.removeItem(STORAGE_KEY)
+
+    try {
+        console.log(savedMessage)
+    } catch {(error)
+    console.log(error.name)
+    console.log(error.message)
+}
+};
+
+function getText() {
+
+    if (savedMessage) {
+        formRef.email.value = savedMessage.email || '';
+        formRef.message.value = savedMessage.message || '';
+    }
+}
+
 
